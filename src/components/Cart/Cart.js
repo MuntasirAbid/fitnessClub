79db871b91a7c5c04de8368119346140decab4df
MuntasirAbid <muntasiralabid@gmail.com>
2022-09-30 18:32:34 +0600

    cart updated

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -21,7 +21,7 @@ const Cart = ({ cart, }) => {
             <button ><p>20</p></button>
             <button><p>30</p></button>
             <button><p>40</p></button>
-            <p>Selected items: {cart.length}</p>
+
             <h2>Exercise Details</h2>
             <p>Exercise time: {total}min</p>
             <p>Break time:  </p>
